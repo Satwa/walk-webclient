095dc4bc109bd38e835144568ccec8cb87210eff
Joshua <satwa.fr@gmail.com>
2017-11-21 22:06:49 +0100

    Travail sur la page directions.html : affichage type Turn-by-Turn, map qui tourne selon l'orientation du téléphone NEXT: Afficher les instructions et changer ses informations de manière dynamique (distance restante, instruction suivante...) NEXT#2: Ajouter ETA (si possible mettre à jour régulièrement, même via un calcul approximatif)

--- assets/directions.js
+++ assets/directions.js
@@ -1,17 +1,17 @@
 var url = new URL(window.location.href);
-//console.log(url.searchParams.get("id"));
 var flying;
+var directionsInfo;
+var geopos;
+
+// console.log(url.searchParams.get("showList") != null);
+
 map.on('flystart', function(){
     flying = true;
-});/*
-map.on('flyend', function(){
-    flying = false;
-});*/
+});
 
 map.on('moveend', function(e){
 	if(!flying){
 		loadWalk();
-		//map.fire(flyend); 
 		flying = true;
    }
 });
@@ -25,29 +25,65 @@ function loadWalk(){
 			}
 			var poiList = JSON.parse(data.path);
 
+			var geopos = {
+				lat: geolocate._lastKnownPosition.coords.latitude,
+				lon: geolocate._lastKnownPosition.coords.longitude
+			};
+
 			// Ici on affiche le trajet
+			var allPos = "";
 			directions.removeRoutes();
-
-			try{
-				var geopos = {
-					lat: geolocate._lastKnownPosition.coords.latitude,
-					lon: geolocate._lastKnownPosition.coords.longitude
-				};
-			}catch(e){ console.log(e); }
-
 			directions.setOrigin([geopos.lon, geopos.lat]);
 			for (var i = 0; i < poiList.length; i++) {
 				if(poiList[i].name === undefined){
 					poiList[i].name = "Your position";
 				}
+				if(i == poiList.length - 1){
+					//Last
+					allPos += poiList[i].lon + "," + poiList[i].lat;
+				}else{
+					allPos += poiList[i].lon + "," + poiList[i].lat + ";";
+				}
 	            directions.addWaypoint(i, [poiList[i].lon, poiList[i].lat]);
 	        }
 	        directions.setDestination([geopos.lon, geopos.lat]);
+	        map.easeTo({
+				pitch: 60,
+				zoom: 18
+	        });
 
-			console.log(poiList);
+	       	//directionsInfo = getDirections(allPos);
+
+			if(window.DeviceOrientationEvent) {
+				window.addEventListener('deviceorientation', function(event){
+					console.log("triggered");
+					let compassdir;
+					if(event.webkitCompassHeading){
+					    compassdir = event.webkitCompassHeading;
+				  	}else{
+				    	compassdir = event.alpha;
+				  	}
+					map.easeTo({
+						center: [geopos.lon, geopos.lat],
+				        pitch: 60,
+					    bearing: 360 - compassdir,
+					});
+				});
+			}
 		})
 		.catch(function(err){
 			alert("An error occured");
 			console.log(err);
 		});
+}
+
+function getDirections(allPos){
+	$.ajax({
+		method: "GET",
+		url: "https://api.mapbox.com/directions/v5/mapbox/walking/" + allPos + "?steps=true&access_token=" + mapboxgl.accessToken
+	})
+	.done(function(data){
+		console.log(data);
+		return data;
+	});
 }
