9fabaea6785086aaed520a9d9751297fe31f2c00
Joshua <satwa.fr@gmail.com>
2018-02-16 19:51:28 +0100

    BUGFIX

--- assets/home.js
+++ assets/home.js
@@ -2,8 +2,8 @@ $('#val').html($("#freeTime").val());
 $(document).on('input', '#freeTime', function() {
     $('#val').html($(this).val());
 });
-let clipboard = new Clipboard('#clipboard');
-clipboard.on('success', (e) => {
+let _clipboard = new Clipboard('#clipboard');
+_clipboard.on('success', (e) => {
 	alert("The link has been copied to clipboard, feel free to share it with your friends!")
 })
 
@@ -11,10 +11,12 @@ let goUrl, geopos
 let allPos = ""
 let allSteps = [],
     stepsLocation = [],
-    stepsIcon = []
+    stepsIcon = [],
+    stepsDuration = []
 let saveData
 let waypoints = ""
-let loadedWalk = false
+let loadingWalk = false
+let loadWalkInterval
 
 let imageStorage = "https://app.walk.cafe/mapbox-directions/"
 
@@ -24,7 +26,6 @@ mapboxgl.accessToken = "pk.eyJ1Ijoic2F0d2F5YSIsImEiOiJjaWsyaTV1NnQwMzRndm5rcGFye
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9?optimize=true',
-    //center: [0, 0],
     zoom: 8,
 });
 
@@ -56,16 +57,26 @@ $(document).ready(function(){
 		// Il existe un id donc on le parse
 		$("#searchBtn").prop('disabled', true);
 		$("#searchBtn").html("Loading...");
-		navigator.geolocation.getCurrentPosition(function(position) {
-		  	geopos = {
-				lat: position.coords.latitude,
-				lon: position.coords.longitude
-			};
-			loadedWalk = true
-			getWalk(getUrlParameter('id')).then(oneWalkCallback).catch((e) => console.error(e))
-		});
+
+		renderOneWalk()
 	}
 });
+
+let renderOneWalk = () => {
+	// Fonction qui est appelée tant que map.loaded() = false
+	if(!loadingWalk){
+		loadingWalk = true
+		loadWalkInterval = setInterval(renderOneWalk, 300)
+	}
+	if(map.loaded()){
+		geopos = {
+			lat: geolocate._lastKnownPosition.coords.latitude,
+			lon: geolocate._lastKnownPosition.coords.longitude
+		}
+		clearInterval(loadWalkInterval)
+		getWalk(getUrlParameter('id')).then(oneWalkCallback).catch((e) => console.error(e))
+	}
+}
 
 $("#searchBtn").click(function(e){
 	e.preventDefault();
@@ -76,7 +87,7 @@ $("#searchBtn").click(function(e){
   	geopos = {
 		lat: geolocate._lastKnownPosition.coords.latitude,
 		lon: geolocate._lastKnownPosition.coords.longitude
-	};
+	}
 	//Add Loading
 	$("#searchBtn").prop('disabled', true);
 	$("#searchBtn").html("Loading...");
@@ -99,43 +110,39 @@ var oneWalkCallback = function(data){
 	delete data.provider;
 	$("#clipboard").attr("data-clipboard-text", 'https://app.walk.cafe/index.html?id=' + data.id)
 	
-	map.once("load", () => {
-		console.log("rendered")
-		waypoints += "<li><b>" + data.duration + " min</b></li>";
-		directions.removeRoutes();
-		directions.setOrigin([geopos.lon, geopos.lat]);
+	waypoints += "<li><b>" + data.duration + " min</b>, <b>" + (data.poi.length - 2) + " POI</b></li>";
+	directions.removeRoutes();
+	directions.setOrigin([geopos.lon, geopos.lat]);
 
-		for (var i = 0; i < data.poi.length; i++) {
-			if(data.poi[i].name === undefined){
-				data.poi[i].name = "Your position";
-			}
-			if(i == data.poi.length - 1){
-				//Last
-				allPos += data.poi[i].lon + "," + data.poi[i].lat;
-			}else{
-				allPos += data.poi[i].lon + "," + data.poi[i].lat + ";";
-			}
-	        directions.addWaypoint(i, [data.poi[i].lon, data.poi[i].lat]);
-	        waypoints += "<li>" + data.poi[i].name + "</li> \n";
-	    }
-	    directions.setDestination([geopos.lon, geopos.lat]);
-	    map.flyTo({
-	        center: [geopos.lon, geopos.lat],
-	        zoom: 13
-	    });
-		$('#popupList #listStyle').html(waypoints);
-		$("#popupSearch").css("display", "none");
-		$("#popupChoice").css("display", "none");
-		$("#popupList").css("display", "block");
-	})
-
-
+	for (var i = 0; i < data.poi.length; i++) {
+		if(data.poi[i].name === undefined){
+			data.poi[i].name = "Your position";
+		}
+		if(i == data.poi.length - 1){
+			//Last
+			allPos += data.poi[i].lon + "," + data.poi[i].lat;
+		}else{
+			allPos += data.poi[i].lon + "," + data.poi[i].lat + ";";
+		}
+        directions.addWaypoint(i, [data.poi[i].lon, data.poi[i].lat]);
+        waypoints += "<li>" + data.poi[i].name + "</li> \n";
+    }
+    directions.setDestination([geopos.lon, geopos.lat]);
+    map.flyTo({
+        center: [geopos.lon, geopos.lat],
+        zoom: 13
+    });
+	$('#popupList #listStyle').html(waypoints);
+	$("#popupSearch").css("display", "none");
+	$("#popupChoice").css("display", "none");
+	$("#popupList").css("display", "block");
 
 }
 
 var walkDataCallback = function(data){
 	$("#searchBtn").prop('disabled', false);
 	$("#searchBtn").html("Search a walk");
+
 	if(data.error === undefined){
 		saveData = data;
 		let i = 1
@@ -202,12 +209,13 @@ var walkDataCallback = function(data){
 			$("#popupList").css("display", "block");
 		})
 	}else{
-    	$('#popupList #listStyle').html("I'm sorry but I found nothing interesting <br> You can suggest a walk at <a href='https://twitter.com/getwalkapp' target='_blank'>@getwalkapp</a>");
+		saveData = null
+    	$('#popupChoice ul').html("I'm sorry but I found nothing interesting <br> You can suggest a walk at <a href='https://twitter.com/getwalkapp' target='_blank'>@getwalkapp</a>");
 	}
+		$("#popupSearch").css("display", "none");
+		$("#popupChoice").css("display", "block");
+		$("#popupList").css("display", "none");
 
-	$("#popupSearch").css("display", "none");
-	$("#popupChoice").css("display", "block");
-	$("#popupList").css("display", "none");
 }
 
 $("#go").click(function(e){
@@ -267,19 +275,21 @@ $(".cancel").click(function(e){
 		$("#popupList").css("display", "none");
 		$("#popupSearch").css("display", "none");
 	}
-	if(loadedWalk){
+	if(loadingWalk){
 		$("#searchBtn").prop('disabled', false);
 		$("#searchBtn").html("Search a walk");
 		$("#popupChoice").css("display", "none");
 		$("#popupList").css("display", "none");
 		$("#popupSearch").css("display", "block");
-		loadedWalk = false
+		loadingWalk = false
 	}
 
 	window.history.pushState("", "", 'index.html');
 	directions.removeRoutes();
 });
 
+let reducer = (acc, cntVal) => acc + cntVal
+let _time = new Date()
 
 function getDirections(allPos){
 	$.ajax({
@@ -287,26 +297,31 @@ function getDirections(allPos){
 		url: "https://api.mapbox.com/directions/v5/mapbox/walking/" + allPos + "?steps=true&access_token=" + mapboxgl.accessToken
 	})
 	.done(function(data){
-   	var steps = data.routes[0].legs[0].steps;
-   	let i = 0, name;
-		steps.forEach(function(step) {
-	    	i++;
+		// console.log(data)
+	   	var legs = data.routes[0].legs
+	   	let i = 0, name
+	   	legs.forEach((leg) => {
+	   		leg.steps.forEach((step) => {
+	   			i++
 
-	    	if(step.maneuver.modifier){
-	    		// Le modifier existe
-	    		name = "direction_" + step.maneuver.type + "_" + step.maneuver.modifier + ".png"
-	    	}else{
-	    		name = "direction_" + step.maneuver.type + ".png"
-	    	}
-	    	stepsIcon.push(name); // All steps icon
-		 	allSteps.push(step.maneuver.instruction); // All steps instruction 
-		 	stepsLocation.push(step.maneuver.location); // All steps position
-		 	
-		 	if(i == steps.length){
-		        $("#information").html("<img src='" + imageStorage + stepsIcon[0] + "'>" + allSteps[0]);
-		        var instructionTimeout = window.setInterval(showCurrentDirection, 1000);
-		 	}
-		});
+		    	if(step.maneuver.modifier){
+		    		// Le modifier existe
+		    		name = "direction_" + step.maneuver.type + "_" + step.maneuver.modifier + ".png"
+		    	}else{
+		    		name = "direction_" + step.maneuver.type + ".png"
+		    	}
+		    	stepsIcon.push(name); // All steps icon
+			 	allSteps.push(step.maneuver.instruction); // All steps instruction 
+			 	stepsLocation.push(step.maneuver.location); // All steps position
+			 	if(step.duration !== 0) stepsDuration.push(step.duration) // All steps duration
+
+			 	if(i == leg.steps.length){
+			 		_time.setTime(_time.getTime() + stepsDuration.reduce(reducer)*1000)
+			        $("#information").html("<img src='" + imageStorage + stepsIcon[0] + "'>" + allSteps[0] + "<span style='float:right'>" + _time.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit'}) + "</span>");
+			        var instructionTimeout = window.setInterval(showCurrentDirection, 1000);
+			 	}
+	   		})
+	   	})
 	});
 }
 
@@ -318,9 +333,19 @@ function showCurrentDirection(){
   	let roundValue = 4; // Default is 4, else is for debug (such as 2)
 	// console.log("(pos) LAT: " + geopos.lat.toFixed(roundValue) + " - LON: " + geopos.lon.toFixed(roundValue));
 	// console.log("(nxt) LAT: " + stepsLocation[0][1].toFixed(roundValue) + "- LON: " + stepsLocation[0][0].toFixed(roundValue));
+	
+	// On met toujours à jour par sécurité, si y'a un bouchon sur le trottoir
+	_time.setTime(new Date().getTime() + stepsDuration.reduce(reducer)*1000)
+	$("#information").html("<img src='" + imageStorage + stepsIcon[0] + "'>" + allSteps[0] + "<span style='float:right'>" + _time.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit'}) + "</span>");
+	
+
 	if(geopos.lat.toFixed(roundValue) === stepsLocation[0][1].toFixed(roundValue) && geopos.lon.toFixed(roundValue) === stepsLocation[0][0].toFixed(roundValue)){
 		// On met à jour l'affichage
-		$("#information").html("<img src='" + imageStorage + stepsIcon[1] + "'>" + allSteps[1]);
+
+		stepsDuration.splice(0, 1)
+		_time.setTime(new Date().getTime() + stepsDuration.reduce(reducer)*1000)
+
+		$("#information").html("<img src='" + imageStorage + stepsIcon[1] + "'>" + allSteps[1] + "<span style='float:right'>" + _time.toLocaleTimeString('fr-FR', {hour: '2-digit', minute: '2-digit'}) + "</span>");
 		
 		// On retire de l'array
 		stepsIcon.splice(0, 1);
@@ -333,5 +358,4 @@ function showCurrentDirection(){
 
 		console.log("Near!");
 	}
-  	// TODO: let ETA = 2; // v = d/t <=> t = d/v  // https://stackoverflow.com/questions/7687884/add-10-seconds-to-a-javascript-date-object-timeobject
 }
